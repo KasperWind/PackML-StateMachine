86c683b844763774335570102055790d76c70988
Kasper Wind <Kasper@Wind.as>
2019-05-24 22:49:30 +0200

    Added runAll and runSingle functions to packML manager

--- StateMachine/StateMachine/PackMLManager.fs
+++ StateMachine/StateMachine/PackMLManager.fs
@@ -2,4 +2,14 @@
 
 open PackML
 
-type Container<'a> = Map< string, PackMLModel<'a> >
+type Container<'a,'b when 'b : comparison> = Map< 'b, PackMLModel<'a> >
+
+let runSingle selector ( container :  Container<'a, 'b> ) : Container<'a, 'b> =
+    container |> Map.tryFind selector
+    |> function
+        | None -> container
+        | Some s -> container |> Map.add selector (s |> run)
+    
+let runAll (container : Container<'a, 'b>) : Container<'a, 'b> =
+    container
+    |> Map.map ( fun _ model -> model |> run )
