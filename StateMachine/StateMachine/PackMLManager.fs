c41e1b5e686ab6d55523541ad03f87fe322db016
Kasper Wind <Kasper@Wind.as>
2019-05-25 09:56:40 +0200

    Updated with tree implementation and added map function.

--- StateMachine/StateMachine/PackMLManager.fs
+++ StateMachine/StateMachine/PackMLManager.fs
@@ -2,11 +2,26 @@
 
 open PackML
 
+//TODO: as node tree?
+type PackMLManager<'a> = 
+    | NoChilds of node: PackMLModel<'a>
+    | Childs of node: PackMLModel<'a> * nodes: PackMLManager<'a> list
+
+module NodeTree =
+    let map (f:PackMLModel<'a> -> PackMLModel<'b>) (x:PackMLManager<'a>) : PackMLManager<'b> =
+        let rec innerF nodetree =
+            match nodetree with
+            | NoChilds node -> f node |> NoChilds
+            | Childs (node, nodes) ->
+                let n = f node
+                let nt = nodes |> List.map innerF
+                (n, nt) |> Childs
+        x |> innerF
+
 type Container<'a,'b when 'a : comparison> = Map< 'a, PackMLModel<'b> >
 
 module Container =
-    let ofList list : Container<'a, 'b>  =
-        list |> Map.ofList
+    let ofList list : Container<'a, 'b>  = list |> Map.ofList
 
 let runSingle selector ( container :  Container<'a, 'b> ) : Container<'a, 'b> =
     container |> Map.tryFind selector
@@ -17,3 +32,7 @@ let runSingle selector ( container :  Container<'a, 'b> ) : Container<'a, 'b> =
 let runAll (container : Container<'a, 'b>) : Container<'a, 'b> =
     container
     |> Map.map ( fun _ model -> model |> run )
+
+// f: b -> c x: Container<a, b>
+let map f x =
+    ()
