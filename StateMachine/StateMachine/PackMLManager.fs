e2e725c3176ee6253c60142fbe0ffbdf62f33bfb
Kasper Wind <Kasper@Wind.as>
2019-05-26 22:00:37 +0200

    Added iter function

--- StateMachine/StateMachine/PackMLManager.fs
+++ StateMachine/StateMachine/PackMLManager.fs
@@ -13,3 +13,15 @@ let rec map ( f: PackMLModel<obj> -> PackMLModel<obj> ) ( x: PackMLManager ) : P
         let n = f me
         let nt = childs |> List.map (map f)
         (n, nt) |> Childs
+        
+let rec iter ( f: PackMLModel<obj> -> unit ) ( x: PackMLManager) : Unit =
+    match x with
+    | NoChilds me -> f me 
+    | Childs (me, childs) ->
+        f me
+        childs |> List.iter (iter f)
+
+let printStates x =
+    let printState ( model: PackMLModel<obj> ) =
+        let name = PackMLModel. model.Context.Name
+    x |> iter |> 
