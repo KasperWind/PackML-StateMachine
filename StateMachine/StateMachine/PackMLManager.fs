d04a19de1c57b5527aaaf1e07f08d8185dd104e3
Kasper Wind <Kasper@Wind.as>
2019-05-26 21:51:05 +0200

    Change container to a tree list and implemented map function

--- StateMachine/StateMachine/PackMLManager.fs
+++ StateMachine/StateMachine/PackMLManager.fs
@@ -2,35 +2,14 @@
 
 open PackML
 
-type PackMLManager<'a> = 
-    | NoChilds of node: PackMLModel<'a>
-    | Childs of node: PackMLModel<'a> * nodes: PackMLManager<'a> list
+type PackMLManager = 
+    | NoChilds of node: PackMLModel<obj>
+    | Childs of node: PackMLModel<obj> * nodes: PackMLManager list
 
-module PackMLManager =
-    let rec map ( f: PackMLModel<'a> -> PackMLModel<'b> ) ( x: PackMLManager<'a> ) : PackMLManager<'b> =
-        match x with
-        | NoChilds me -> f me |> NoChilds
-        | Childs (me, childs) ->
-            let n = f me
-            let nt = childs |> List.map (map f)
-            (n, nt) |> Childs
-(*        
-type Container<'a,'b when 'a : comparison> = Map< 'a, PackMLModel<'b> >
-
-module Container =
-    let ofList list : Container<'a, 'b>  = list |> Map.ofList
-
-let runSingle selector ( container :  Container<'a, 'b> ) : Container<'a, 'b> =
-    container |> Map.tryFind selector
-    |> function
-        | None -> container
-        | Some s -> container |> Map.add selector (s |> run)
-    
-let runAll (container : Container<'a, 'b>) : Container<'a, 'b> =
-    container
-    |> Map.map ( fun _ model -> model |> run )
-
-// f: b -> c x: Container<a, b>
-let map f x =
-    ()
-    *)
+let rec map ( f: PackMLModel<obj> -> PackMLModel<obj> ) ( x: PackMLManager ) : PackMLManager =
+    match x with
+    | NoChilds me -> f me |> NoChilds
+    | Childs (me, childs) ->
+        let n = f me
+        let nt = childs |> List.map (map f)
+        (n, nt) |> Childs
